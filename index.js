4c16f30d73667f0bfef38360192f79fb3a8865a8
Florian Blandin <pro.blandinf@gmail.com>
2020-01-21 19:38:09 +0100

    user should now be able to selects tracks on a predefined list

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
 require("dotenv").config();
 
-const { pipeline } = require("stream")
 const request = require("request")
-const tweetParser = require("./tweetParser")
-const logger = require("./logger")
 const splitter = require("./splitter")
 const { server, wsServer } = require("./server")
 const WsWriter = require("./wsWriter")
 
-const httpRequestStream = request.post(`${process.env.TWITTER_API_URL}/statuses/filter.json`, {
-    json: true,
-    form: {
-        track: "javascript, nodejs"
-    },
-    oauth: {
-        consumer_key: process.env.TWITTER_API_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_API_CONSUMER_SECRET,
-        token: process.env.TWITTER_API_TOKEN,
-        token_secret: process.env.TWITTER_API_TOKEN_SECRET
-    }
-}).on('error', (error) => {
-    console.error(error)
-})
-
-const twitterStream = httpRequestStream.pipe(splitter)
-                                    //    .pipe(tweetParser)
-
-wsServer.on("connection", (socket) => {
-    socket.send('coucou')
-    socket.on("message", (message) => {
-        console.log('message du client:' + message)
+function launchRequest (track, socket) {
+    const httpRequestStream = request.post(`${process.env.TWITTER_API_URL}/statuses/filter.json`, {
+        json: true,
+        form: {
+            track: track
+        },
+        oauth: {
+            consumer_key: process.env.TWITTER_API_CONSUMER_KEY,
+            consumer_secret: process.env.TWITTER_API_CONSUMER_SECRET,
+            token: process.env.TWITTER_API_TOKEN,
+            token_secret: process.env.TWITTER_API_TOKEN_SECRET
+        }
+    }).on('error', (error) => {
+        console.error(error)
     })
+
+    const twitterStream = httpRequestStream.pipe(splitter)
     const wsWriter = new WsWriter(socket)
     twitterStream.pipe(wsWriter)
+}
+
+wsServer.on("connection", (socket) => {
+    socket.on("message", (message) => {
+        if (message) {
+            launchRequest(message, socket)
+        }
+    })
 })
 
 server.listen(process.env.PORT)
